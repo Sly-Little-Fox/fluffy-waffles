9a22985868c6de2fb3bff785d03bef03eb6c8df2
Sly-Little-Fox <69681150+Sly-Little-Fox@users.noreply.github.com>
2020-11-29 23:08:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+const LIMIT = 143859; <!--All Unicode characters-->
+
+var out = "";
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+for(let i = 0; i < limit; i++){
+  out += String.fromCharCode(i);
+  out += '\n';
+}
+
+download("unicode.txt", out)
